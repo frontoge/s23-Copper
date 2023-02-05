facd019825545d77830e0bd090bcde65a5769dbf
alhull <ahull001@odu.edu>
2023-02-05 15:52:19 -0500

    added logIn component

--- grocery-genie/src/index.js
+++ grocery-genie/src/index.js
@@ -1,16 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import logIn from './components/logIn';
+
 
 class App extends React.Component {
     render(){
         return (
             <div>
-                Content is here!
+                <logIn/>
             </div>
         )
     }
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <App />
+    <App/>
 )
