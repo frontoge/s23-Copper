08e741ba9ab368ae9dfb562823a687f5746cc7d2
aricat15 <aricat15@gmail.com>
2023-02-16 01:33:28 -0500

    UI Login View

---
+++ grocery-genie/src/main/java/ui.login/LoginUserView.java
@@ -0,0 +1,17 @@
+package main.java.ui.login;
+
+class LoginUserView 
+{
+
+    private String displayName;
+
+    LoginUserView(String displayName) {this.displayname = displayName;}
+
+    String getDisplayName(){return displayName;}
+
+
+
+
+
+
+}
