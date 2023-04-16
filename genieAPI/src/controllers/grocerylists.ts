fee227dcfec0bc1a6ec227946ea222358c806b7e
flin002 <flin002@odu.edu>
2023-04-16 15:34:06 -0400

    groceryListAPI

---
+++ genieAPI/src/controllers/grocerylists.ts
@@ -0,0 +1,9 @@
+import {Request, Response, NextFunction} from "express";
+import db from "../db_connect";
+
+const getGroceryList = (req: Request, res: Response, next: NextFunction)=>{
+    db.connect((err: Error) =>{
+        if (err) throw err;
+        db.query(`SELECT item, quantity, active FROM grocerylist WHERE owner = ${req.params.userID}`)
+    })
+}
