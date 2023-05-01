35c2ecf8d6dc0494835f031c11e3c1bb9b7de6c5
Erica Lee <elee006@odu.edu>
2023-05-01 10:20:33 -0400

    sorting working without styling

--- grocery-genie/src/components/storeRecs.jsx
+++ grocery-genie/src/components/storeRecs.jsx
@@ -15,10 +15,10 @@ import target from '../images/target_logo.png'
 <link rel="stylesheet" href="../styles/storeRecStyles.css"></link>
 
 const storeList = [
-    {name:'Walmart',image:walmart, location: 5.5, price:100, in_stock: 12, link: 'https://www.walmart.com/cp/976759?&adid=22222222220221176149&wmlspartner=wmtlabs&wl0=e&wl1=g&wl2=c&wl3=193590193349&wl4=aud-1651068664266:kwd-667579415&wl5=9008553&wl6=&wl7=&wl8=&veh=sem&gclid=CjwKCAjwrpOiBhBVEiwA_473dBKP0ffG4wqCJX38ZbJUwECrYGszdswQGq1i8Ch47dpGU1S_GgsylRoCeEoQAvD_BwE&gclsrc=aw.ds'},
-    {name:'Kroger', image:kroger, location : 6.5, price:101, in_stock: 10,link: 'https://www.kroger.com/pr/pickup-delivery-savings-4?ds_rl=1281562&ds_rl=1281680&cid=ps_adw_ogs_15x1savoffer_t:krogerclick&gclid=CjwKCAjwrpOiBhBVEiwA_473dFDZrwW3aTGjQaQwviSQy7rjNQwTiJkl1vDpK0YPb9cE5A0d8EM16xoCN20QAvD_BwE&gclsrc=aw.ds'},
-    {name:'Aldi', image: aldi, location: 1.7, price:99, in_stock: 9,link: 'https://www.aldi.us/?utm_source=google&utm_medium=sem&utm_campaign=brand&utm_term=aldi&gclid=CjwKCAjwrpOiBhBVEiwA_473dMx0IJIjK0_ys11If1jVm80YPOLbXhfDPAlyt26YwahkrRHvkiCNfRoCtAgQAvD_BwE&gclsrc=aw.ds'},
-    {name: 'Target', image: target, location: 8.4, price: 70, in_stock: 17,link: 'https://www.target.com/c/grocery/-/N-5xt1a'}
+    {id: 1, name:'Walmart',image:walmart, location: 5.5, price:100, in_stock: 12, link: 'https://www.walmart.com/cp/976759?&adid=22222222220221176149&wmlspartner=wmtlabs&wl0=e&wl1=g&wl2=c&wl3=193590193349&wl4=aud-1651068664266:kwd-667579415&wl5=9008553&wl6=&wl7=&wl8=&veh=sem&gclid=CjwKCAjwrpOiBhBVEiwA_473dBKP0ffG4wqCJX38ZbJUwECrYGszdswQGq1i8Ch47dpGU1S_GgsylRoCeEoQAvD_BwE&gclsrc=aw.ds'},
+    {id: 2, name:'Kroger', image:kroger, location : 6.5, price:101, in_stock: 10,link: 'https://www.kroger.com/pr/pickup-delivery-savings-4?ds_rl=1281562&ds_rl=1281680&cid=ps_adw_ogs_15x1savoffer_t:krogerclick&gclid=CjwKCAjwrpOiBhBVEiwA_473dFDZrwW3aTGjQaQwviSQy7rjNQwTiJkl1vDpK0YPb9cE5A0d8EM16xoCN20QAvD_BwE&gclsrc=aw.ds'},
+    {id: 3, name:'Aldi', image: aldi, location: 1.7, price:99, in_stock: 9,link: 'https://www.aldi.us/?utm_source=google&utm_medium=sem&utm_campaign=brand&utm_term=aldi&gclid=CjwKCAjwrpOiBhBVEiwA_473dMx0IJIjK0_ys11If1jVm80YPOLbXhfDPAlyt26YwahkrRHvkiCNfRoCtAgQAvD_BwE&gclsrc=aw.ds'},
+    {id: 4, name: 'Target', image: target, location: 8.4, price: 70, in_stock: 17,link: 'https://www.target.com/c/grocery/-/N-5xt1a'}
     ];
 let s = [];
 
@@ -72,7 +72,6 @@ function StoreRec(props) {
         return (
         <div className='storeinfo'>
               
-                <h2>Sort</h2>
                 <select onChange={(e) => setSortType(e.target.value)}>
                     <option value="location">Location</option>
                     <option value="price">Price</option>
@@ -82,53 +81,16 @@ function StoreRec(props) {
            
        
            
-        {data?.map((store, index) => {
-        s.push(           
-        <div>
-            <Box
-            sx={{
-            width: 300,
-            height: 300,
-            position: 'relative',
-            backgroundColor: 'primary.main'}}>
-        <img src={data[index].image} alt="target" width={150} height={100}
-        sx={{
-            position: 'absolute',
-            top:0,
-            left:0
-        }}/>
-        
-          <div  sx={{
-                position: 'relative',
-                top:0,
-                end:0
-            }}>
-            
-        <h5> {data[index].location} mi</h5>
+    {data.map(store => (
+        <div key={store.id} style={{ margin: '30px' }}>
+          <div>{`store: ${store.name}`}</div>
+          <div>{`location: ${store.location} mi`}</div>
+          <div>{`price: ${store.price}`}</div>
+          <div>{`instock: ${store.in_stock}`}</div>
         </div>
-            
-            <p>
-                                In Stock Items: {data[index].in_stock}
-                            </p>
-                            <p>
-                                Total Price: ${data[index].price}
-                            </p>
-            <Button variant="contained" 
-            sx={{
-                position: 'absolute',
-                right:0,
-                bottom:0
-            }}onClick={(e)=> {
-                window.location.href=data[index].link
-            }}>Go to {data[index].name}</Button>
-            </Box>
-            <h2> </h2>
-            </div>
-            
-    
-        )
-        })}
-            <ul key="store" className ='storeList'> {s} </ul>
+      ))}
+       
+        
                 </div>
             
 /*
