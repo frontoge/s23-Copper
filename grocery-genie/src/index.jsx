66589e3e4a1db71e68ae7d2416c97e23de88f736
Frontoge <frontoge@gmail.com>
2023-04-30 20:55:14 -0400

    Add recipe card component

--- grocery-genie/src/index.jsx
+++ grocery-genie/src/index.jsx
@@ -6,7 +6,6 @@ import {ThemeProvider} from "@mui/material/styles"
 import theme from "./theme";
 import LogIn from "./login/logIn";
 import GroceryList from "./groceries/groceryList";
-// import Recipe from "./components/recipe";
 import MealPlan from "./mealplan/mealPlan";
 import StoreRec from "./stores/storeRecs";
 import Account from "./login/account"
@@ -14,17 +13,20 @@ import {Settings} from "./extras/components/settings";
 import Upload from "./recipes/upload";
 import Household_Profile from "./household/household";
 import Recipe from "./recipes/recipe";
+import RecipeCard from "./recipes/components/recipeCard";
+import CreateRecipe from "./recipes/components/createRecipe";
 
 function App(props) {
     return (
         <BrowserRouter>
         <Routes>
-            <Route path={"/login"} index element={<LogIn />} />
-            <Route path={"account"} element={<Account />}></Route>
+            <Route path={"/login"} element={<LogIn />} />
+            <Route path={"/account"} element={<Account />}></Route>
             <Route path="/" element={<Layout />}>
+                <Route index element={<Household_Profile />}></Route>
                 <Route path={"grocerylist"} element={<GroceryList />}></Route>
-                <Route path={"household"} element={<Household_Profile />}></Route>
                 <Route path={"recipes"} element={<Recipe />}></Route>
+                <Route path={"recipes/create"} element={<CreateRecipe/>}></Route>
                 <Route path={"upload"} element={<Upload />}></Route>
                 <Route path={"mealplan"} element={<MealPlan />}></Route>
                 <Route path={"stores"} element={<StoreRec />}></Route>
---
+++ grocery-genie/src/recipes/components/recipeCard.jsx
@@ -0,0 +1,80 @@
+import { Typography, Card, Divider, CardHeader, CardMedia, CardContent, CardActions, IconButton, Collapse, ListItem, List } from "@mui/material";
+import React from "react";
+import Template from '../../images/garlicButterWing.jpg'
+import "../styles/recipeCard.css"
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import { styled } from '@mui/material/styles';
+
+//This is taken from the example on the MUI docs https://mui.com/material-ui/react-card/
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
+
+export default function RecipeCard(props) {
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+    
+    return (
+        <Card sx={{
+            width: "25rem",
+            alignItems: "center",
+        }}>
+            <CardHeader title={props.name}/>
+            <CardMedia 
+            component="img" 
+            height={194}
+            image={`data:image/jpeg;base64,${props.image}`}
+            alt="Recipe image"
+            />
+            <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                    {props.description}
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                </IconButton>
+                <IconButton aria-label="share">
+                    <ShareIcon />
+                </IconButton>
+                <ExpandMore
+                    expand={expanded}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                >
+                    <ExpandMoreIcon />
+                </ExpandMore>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit >
+                <CardContent>
+                    <Typography variant="h5" sx={{marginBottom: "0.1rem"}}>Ingredients:</Typography>
+                    <List sx={{paddingTop: "0"}}>
+                        {props.ingredients.map((value) =>
+                            <Typography ListItem>{value.quantity}{value.units} {value.name}</Typography>
+                        )}
+                    </List>
+                    <Typography variant="h5" sx={{marginBottom: "0.1rem", marginTop: "0.5rem"}}>Steps</Typography>
+                    <List sx={{paddingTop: "0"}}>
+                        {props.steps.map((value, index) =>
+                            <Typography ListItem>{index + 1}. {value}</Typography>
+                        )}
+                    </List>
+                </CardContent>
+            </Collapse>
+        </Card>
+    )
+}
