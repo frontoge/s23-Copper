0ea045df37bc1e1bad27e0e9ff1d5dff12715399
alhull <ahull001@odu.edu>
2023-05-02 13:09:30 -0400

    household profiles rerender when changed

--- grocery-genie/src/components/household.jsx
+++ grocery-genie/src/components/household.jsx
@@ -1,28 +1,12 @@
 import React, { useEffect, useState, useRef } from "react";
-import AddCircleIcon from "@mui/icons-material/AddCircle";
-import IconButton from "@mui/material/IconButton";
-import Input from "@mui/material/Input";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Switch from "@mui/material/Switch";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from "@mui/material/FormControl";
-import FormLabel from "@mui/material/FormLabel";
 import DeleteIcon from "@mui/icons-material/Delete";
-import Divider from "@mui/material/Divider";
-import Typography from "@mui/material/Typography";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
-import Grid from "@mui/material/Unstable_Grid2";
-import { styled } from "@mui/material/styles";
-import { createTheme } from "@mui/material/styles";
 import TextField from "@mui/material/TextField";
-import Stack from "@mui/material/Stack";
-import { Icon } from "@mui/material";
-import {createSearchParams, useNavigate} from "react-router-dom"
 import Cookies from "universal-cookie"
 
 function Household_Profile() {
@@ -57,6 +41,7 @@ function Household_Profile() {
        .then(response => response.json())
        .then(data => {
          console.log(data.message)
+         window.location.reload(false);
        })
        .catch((err) => {
          console.log(err.message)
@@ -82,7 +67,6 @@ function Household_Profile() {
           justifyContent: "center",
         }}
       >   
-       <form onSubmit={handleClickEvent}>
         <TextField
           style={{
             width: "20%",
@@ -144,8 +128,10 @@ function Household_Profile() {
             setMemberInfo({ ...memberInfo, allergy: e.target.value });
           }}
         />
-         <input type="submit" value="Submit" />
-         </form >
+      <Button variant="contained"
+      onClick={(e) => {handleClickEvent(e)}}>
+        Create Profile
+      </Button>
         
       </Box>
     );
@@ -229,6 +215,7 @@ function Household_Profile() {
     .then(response => response.json())
        .then(data => {
          console.log(data.message)
+         window.location.reload(false);
        })
        .catch((err) => {
          console.log(err.message)
@@ -253,6 +240,7 @@ function Household_Profile() {
        .then(data => {
          console.log(data.message)
          console.log("status is ", status)
+         window.location.reload(false);
        })
        .catch((err) => {
          console.log(err.message)
@@ -281,6 +269,7 @@ function Household_Profile() {
     .then(response => response.json())
        .then(data => {
          console.log(data.message)
+         window.location.reload(false);
        })
        .catch((err) => {
          console.log(err.message)
