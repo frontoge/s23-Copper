d9b451f91d8b49bfbaae62cad95c41ce5ce4c5aa
John Doe <johndoe@email.com>
2023-05-02 11:56:42 -0400

    Exctract feature routes to to db

--- grocery-genie/src/components/UrlRecipe.jsx
+++ grocery-genie/src/components/UrlRecipe.jsx
@@ -22,7 +22,7 @@ const UrlRecipe = (props) => {
     const [type, setType] = useState(null)
     const [date, setDate] = useState(null)
     
-    unction addToMealPlan() {
+    function addToMealPlan() {
         fetch(`http://localhost:4000/api/mealplans/`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
@@ -53,9 +53,225 @@ const UrlRecipe = (props) => {
         console.log("mealId ", mealId);
         setShowPopUp(true);
       }
-      
-    return (
 
+      function addRecipe() {
+        console.log(
+          "add Recipe",
+          JSON.stringify(props.extractData.analyzedInstructions)
+        );
+  
+        fetch(`http://localhost:4000/api/recipes/`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            owner: userData.id,
+            name: props.extractData.title,
+            descrption: JSON.stringify(props.extractData.analyzedInstructions),
+            ingredients: JSON.stringify(props.extractData.extendedIngredients),
+            img: props.extractData.image,
+          }),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data.message);
+          })
+          .catch((err) => {
+            console.log(err.message);
+          });
+      }
+
+      function getSub(subString) {
+        console.log("get sub ", subString)
+        
+        fetch(
+          `https://api.spoonacular.com/food/ingredients/substitutes?apiKey=28ae15fcd30248a7bdf22580850a23be&&ingredientName=${subString}`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.status === "success") 
+            setSubList(data);
+            console.log("sub function ", data)
+          })
+          .catch(() => {
+            console.log("error");
+          });
+      }
+      function check() {
+        setShowIngredients(true)
+      }
+
+      const clickHandler = () => {
+        if (icon.current.style.color === "gray")
+          icon.current.style.color = "red";
+        else 
+          icon.current.style.color = "gray";
+      };
+
+    return (
+        <>
+        {props.extractData ? (
+          <Card //className={styles.root}
+            sx={{
+              width: "350px",
+              boxShadow: "10px 10px 5px #afcfcf",
+              marginBottom: "15px",
+              backgroundColor: "#ffffff",
+            }}
+          >
+            <CardHeader
+              sx={{
+                backgroundColor: "#ffffff",
+                color: "#000000",
+                height: "50px",
+              }}
+              title={props.extractData.title}
+              onClick={() => {
+                clickHandler();
+              }}
+              action={
+                <IconButton aria-label="settings">
+                  <FavoriteIcon sx={{ color: "gray" }} ref={icon}/>
+                </IconButton>
+              }
+            />
+            <CardMedia
+              image={props.extractData.image}
+              title={props.extractData.title}
+              sx={{
+                height: 0,
+                paddingTop: "56.25%", // 16:9,
+                marginTop: "20px",
+                marginBottom: "0",
+              }}
+              onClick={() => {
+                updateMeal(props.extractData.title, props.extractData.title, props.extractData.servings);
+              }}
+            />
+            <CardContent sx={{height: "10px", padding: "10px"}}>
+            <Button
+              style={{
+                marginRight: "20px",
+                postion: "relative",
+                bottom: "20px",
+                right: "10px",
+              }}
+              onClick={() => {
+                addRecipe();
+              }}
+            >
+              Save
+            </Button>
+            <Button
+              style={{
+                marginRight: "20px",
+                postion: "relative",
+                bottom: "20px",
+                right: "10px",
+              }}
+              onClick={() => {
+                check()
+              }}
+            >
+              Check Ingredients
+            </Button></CardContent>
+          </Card>
+        ) : null}
+<Box sx={{displayDirection: "column", position: "fixed", top: "20%", left: "30%", backgroundColor: "white", padding: "10px", border: "1px solid #afcfcf"}}>
+        {showIngredients ? (
+          
+          props.extractData.extendedIngredients.map((item) => (
+            <Box >
+              <ListItem sx={{fontWeight: "bold", fontSize: "20px"}}onClick={() => {getSub(item.nameClean)}}>{ item.nameClean }</ListItem>
+            </Box>
+          )
+          )) : null
+        }</Box>
+
+          <div className="subs">
+          {subList ? (
+            <>
+              <h2 className="title">Substitutions</h2>
+              <h2 className="title">{subList.ingredient}</h2>
+              <hr></hr>
+            </>
+          ) : null}
+          {subList
+            ? subList.substitutes.map((subs, index) => (
+                <>
+                  <p className="title" onClick={() => {
+                   setSubList(null)
+                   setShowIngredients(false)
+                  }} key={index}>
+                    {subs} {index}
+                  </p>
+
+                  <br></br>
+                </>
+              ))
+            : null}
+            {showPopUp ? (
+          <Box style={{position: "fixed", top: "30%", left: '25%', backgroundColor: "#ffffff"}} >
+            <TextField
+              label={"Date YYYY-MM-DD"}
+              value={date}
+              onChange={(e) => {
+                setDate(e.target.value);
+              }}
+              sx={{
+                width: "45%",
+                marginRight: "20px",
+                "& .MuiInputLabel-root": { fontSize: "20px", color: "black" }, //styles the label
+                input: {
+                  color: "#468656",
+                  fontWeight: "bold",
+                  fontSize: "1em",
+                },
+              }}
+              style={{
+                border: "1px dotted #79b989",
+                borderRadius: "5px",
+                backgroundColor: "white",
+              }}
+            ></TextField>
+            <TextField
+              label={"Type i.e Breakfast Lunch or Dinner"}
+              value={type}
+              onChange={(e) => {
+                setType(e.target.value);
+              }}
+              sx={{
+                width: "45%",
+                marginRight: "20px",
+                "& .MuiInputLabel-root": { fontSize: "20px", color: "black" }, //styles the label
+                input: {
+                  color: "#468656",
+                  fontWeight: "bold",
+                  fontSize: "1em",
+                },
+              }}
+              style={{
+                border: "1px dotted #79b989",
+                borderRadius: "5px",
+                backgroundColor: "white",
+              }}
+            ></TextField>
+            <Button
+              style={{
+                backgroundColor: "#afcfcf",
+                color: "white",
+                margin: "0 10px",
+              }}
+              onClick={() => {
+                addToMealPlan();
+              }}
+            >
+              Add To Plan
+            </Button>
+          </Box>
+        ) : null}
+        </div>
+
+      </>
     );
 };
 
