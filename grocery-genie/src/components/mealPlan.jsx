6a72498ecc444f3ce73bdc913c1e624e5d5edc0a
jpark032 <jpark032@odu.edu>
2023-05-01 21:11:06 -0400

    date range

--- grocery-genie/src/components/mealPlan.jsx
+++ grocery-genie/src/components/mealPlan.jsx
@@ -7,4 +7,15 @@ import Box from "@mui/material/Box";
 import ListItem from "@mui/material/ListItemText";
 import Button from "@mui/material/Button";
 
+function SetDate(start){
+  const date = new Date()
+  if (start){
+    date.setDate(date.getDate() - date.getDay())
+  }
+  else {
+    date.setDate(date.getDate() + (6 - date.getDay()))
+  }
+  return date
+}
+
 function MealPlan() {
