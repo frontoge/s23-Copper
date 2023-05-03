1f0bf1d975170ab76b4d66f2f4c9b0010ed618a9
unknown <frontoge@gmail.com>
2023-05-03 11:52:08 -0400

    Fix CORS header parsing for options requests

--- genieAPI/src/server.ts
+++ genieAPI/src/server.ts
@@ -30,7 +30,7 @@ router.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
     // set the CORS method headers
     if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST');
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, DELETE, POST');
         return res.status(200).json({});
     }
     next();
